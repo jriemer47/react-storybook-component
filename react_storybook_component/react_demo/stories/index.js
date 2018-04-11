853b9a723cea72db839c3d236df642ba998e1707
Jonathan Riemer <jriemer@Jonathans-MacBook-Pro-2.local>
2018-04-10 22:23:06 -0700

    storybook component created

---
+++ react_storybook_component/react_demo/stories/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Timer from '../src/components/Timer.js';
+import {
+  storiesOf
+} from '@storybook/react';
+
+storiesOf('Timer', module).add('Show', () => < Timer / > );
